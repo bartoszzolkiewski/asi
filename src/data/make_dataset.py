e5b51003699b6852fe92331b99778e30642244b6
koziolm <mkzl@mkzl.pl>
2023-06-08 01:47:09 +0200

    uploaded and debugged notebook (should run now), finished 'make data' part of refactor, updated dependencies

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,7 +3,9 @@ import click
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
-
+import pandas as pd
+import numpy as np
+import csv
 
 @click.command()
 @click.argument('input_filepath', type=click.Path(exists=True))
@@ -13,8 +15,158 @@ def main(input_filepath, output_filepath):
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
 
+    logger.info('making final data set from raw data')
+    logger.info('reading data')
+    application_data = pd.read_csv(r'data/raw/application_data.csv')
+    previous_application = pd.read_csv(r'data/raw/previous_application.csv')
+    columns_description = pd.read_csv(r'data/raw/columns_description.csv',skiprows=1)
+
+    
+
+
+
+    logger.info('processing data')
+    #Removing columns with missing values more than 50%
+    previous_application=previous_application.drop([ 'AMT_DOWN_PAYMENT', 'RATE_DOWN_PAYMENT', 'RATE_INTEREST_PRIMARY',
+    "RATE_INTEREST_PRIVILEGED"],axis=1)
+    #Removing columns with missing values more than 50%
+    application_data=application_data.drop([ 'EXT_SOURCE_1', 'EXT_SOURCE_2', 'EXT_SOURCE_3',
+    'APARTMENTS_AVG', 'BASEMENTAREA_AVG', 'YEARS_BEGINEXPLUATATION_AVG',
+    'YEARS_BUILD_AVG', 'COMMONAREA_AVG', 'ELEVATORS_AVG', 'ENTRANCES_AVG',
+    'FLOORSMAX_AVG', 'FLOORSMIN_AVG', 'LANDAREA_AVG',
+    'LIVINGAPARTMENTS_AVG', 'LIVINGAREA_AVG', 'NONLIVINGAPARTMENTS_AVG',
+    'NONLIVINGAREA_AVG', 'APARTMENTS_MODE', 'BASEMENTAREA_MODE',
+    'YEARS_BEGINEXPLUATATION_MODE', 'YEARS_BUILD_MODE', 'COMMONAREA_MODE',
+    'ELEVATORS_MODE', 'ENTRANCES_MODE', 'FLOORSMAX_MODE', 'FLOORSMIN_MODE',
+    'LANDAREA_MODE', 'LIVINGAPARTMENTS_MODE', 'LIVINGAREA_MODE',
+    'NONLIVINGAPARTMENTS_MODE', 'NONLIVINGAREA_MODE', 'APARTMENTS_MEDI',
+    'BASEMENTAREA_MEDI', 'YEARS_BEGINEXPLUATATION_MEDI', 'YEARS_BUILD_MEDI',
+    'COMMONAREA_MEDI', 'ELEVATORS_MEDI', 'ENTRANCES_MEDI', 'FLOORSMAX_MEDI',
+    'FLOORSMIN_MEDI', 'LANDAREA_MEDI', 'LIVINGAPARTMENTS_MEDI',
+    'LIVINGAREA_MEDI', 'NONLIVINGAPARTMENTS_MEDI', 'NONLIVINGAREA_MEDI',
+    'FONDKAPREMONT_MODE', 'HOUSETYPE_MODE', 'TOTALAREA_MODE',
+    'WALLSMATERIAL_MODE', 'EMERGENCYSTATE_MODE',"OWN_CAR_AGE","OCCUPATION_TYPE"],axis=1)
+    #join csvs
+    mergeddf =  pd.merge(application_data,previous_application,on='SK_ID_CURR')
+    
+    #dropping SK_ID_CURR since it all unique values
+    mergeddf.drop(labels=['SK_ID_CURR'], axis=1, inplace = True)
+    
+    #Now we will take care of null values in each column one by one.
+    
+    enq_cs =['AMT_REQ_CREDIT_BUREAU_DAY', 'AMT_REQ_CREDIT_BUREAU_HOUR',
+        'AMT_REQ_CREDIT_BUREAU_MON', 'AMT_REQ_CREDIT_BUREAU_QRT',
+        'AMT_REQ_CREDIT_BUREAU_WEEK', 'AMT_REQ_CREDIT_BUREAU_YEAR']
+    for i in enq_cs:
+        mergeddf[i] = mergeddf[i].fillna(0)
+
+    amt_cs = ["AMT_ANNUITY_y","AMT_GOODS_PRICE_y"]
+    for i in amt_cs:
+        mergeddf[i] = mergeddf[i].fillna(mergeddf[i].mean())
+
+    cols = ["DAYS_FIRST_DRAWING","DAYS_FIRST_DUE","DAYS_LAST_DUE_1ST_VERSION",
+            "DAYS_LAST_DUE","DAYS_TERMINATION",'CNT_PAYMENT']
+    for i in cols :
+        mergeddf[i]  = mergeddf[i].fillna(mergeddf[i].median())
+        
+    cols = ["NAME_TYPE_SUITE_y","NFLAG_INSURED_ON_APPROVAL"]
+    for i in cols :
+        mergeddf[i]  = mergeddf[i].fillna(mergeddf[i].mode()[0])
+
+    # Rest missing values are under 1.5% so we can drop these rows.
+    mergeddf.dropna(inplace = True)
+
+    # List of variables to map
+
+    varlist =  ['FLAG_OWN_CAR','FLAG_OWN_REALTY','FLAG_LAST_APPL_PER_CONTRACT']
+
+    # Defining the map function
+    def binary_map(x):
+        return x.map({'Y': 1, "N": 0})
+
+    # Applying the function to the housing list
+    mergeddf[varlist] = mergeddf[varlist].apply(binary_map)
+
+    #dropping SK_ID_PREV since non required technical field 
+
+    mergeddf.drop(['SK_ID_PREV'], axis=1, inplace = True)
+    mergeddf.head()
+
+    mergeddf[['FLAG_OWN_CAR','FLAG_OWN_REALTY','FLAG_MOBIL',
+'FLAG_EMP_PHONE','FLAG_WORK_PHONE','FLAG_CONT_MOBILE',
+'FLAG_PHONE','FLAG_EMAIL','REGION_RATING_CLIENT',
+'REGION_RATING_CLIENT_W_CITY','REG_REGION_NOT_LIVE_REGION',
+'REG_REGION_NOT_WORK_REGION','LIVE_REGION_NOT_WORK_REGION',
+'REG_CITY_NOT_LIVE_CITY','REG_CITY_NOT_WORK_CITY','FLAG_DOCUMENT_2',
+'FLAG_DOCUMENT_3','FLAG_DOCUMENT_4','FLAG_DOCUMENT_5','FLAG_DOCUMENT_6',
+'FLAG_DOCUMENT_7','FLAG_DOCUMENT_8','FLAG_DOCUMENT_9',
+'FLAG_DOCUMENT_10','FLAG_DOCUMENT_11','FLAG_DOCUMENT_12','FLAG_DOCUMENT_13',
+'FLAG_DOCUMENT_14','FLAG_DOCUMENT_15','FLAG_DOCUMENT_16','FLAG_DOCUMENT_17',
+'FLAG_DOCUMENT_18','FLAG_DOCUMENT_19','FLAG_DOCUMENT_20','FLAG_DOCUMENT_21',
+'NFLAG_INSURED_ON_APPROVAL']]= mergeddf[['FLAG_OWN_CAR','FLAG_OWN_REALTY','FLAG_MOBIL',
+                                    'FLAG_EMP_PHONE','FLAG_WORK_PHONE','FLAG_CONT_MOBILE',
+                                    'FLAG_PHONE','FLAG_EMAIL','REGION_RATING_CLIENT',
+                                    'REGION_RATING_CLIENT_W_CITY','REG_REGION_NOT_LIVE_REGION',
+                                    'REG_REGION_NOT_WORK_REGION','LIVE_REGION_NOT_WORK_REGION',
+                                    'REG_CITY_NOT_LIVE_CITY','REG_CITY_NOT_WORK_CITY','FLAG_DOCUMENT_2',
+                                    'FLAG_DOCUMENT_3','FLAG_DOCUMENT_4','FLAG_DOCUMENT_5','FLAG_DOCUMENT_6',
+                                    'FLAG_DOCUMENT_7','FLAG_DOCUMENT_8','FLAG_DOCUMENT_9',
+                                    'FLAG_DOCUMENT_10','FLAG_DOCUMENT_11','FLAG_DOCUMENT_12','FLAG_DOCUMENT_13',
+                                    'FLAG_DOCUMENT_14','FLAG_DOCUMENT_15','FLAG_DOCUMENT_16','FLAG_DOCUMENT_17',
+                                    'FLAG_DOCUMENT_18','FLAG_DOCUMENT_19','FLAG_DOCUMENT_20','FLAG_DOCUMENT_21',
+                                    'NFLAG_INSURED_ON_APPROVAL']].astype('category')
+
+    obj_dtypes = [i for i in mergeddf.select_dtypes(include=object).columns if i not in ["type"] ]
+    num_dtypes = [i for i in mergeddf.select_dtypes(include = np.number).columns if i not in [ 'TARGET']]
+
+    # Creating a dummy variable for some of the categorical variables and dropping the first one.
+    dummy1 = pd.get_dummies(mergeddf[['FLAG_OWN_CAR','FLAG_OWN_REALTY','FLAG_MOBIL',
+                                    'FLAG_EMP_PHONE','FLAG_WORK_PHONE','FLAG_CONT_MOBILE',
+                                    'FLAG_PHONE','FLAG_EMAIL','REGION_RATING_CLIENT',
+                                    'REGION_RATING_CLIENT_W_CITY','REG_REGION_NOT_LIVE_REGION',
+                                    'REG_REGION_NOT_WORK_REGION','LIVE_REGION_NOT_WORK_REGION',
+                                    'REG_CITY_NOT_LIVE_CITY','REG_CITY_NOT_WORK_CITY','FLAG_DOCUMENT_2',
+                                    'FLAG_DOCUMENT_3','FLAG_DOCUMENT_4','FLAG_DOCUMENT_5','FLAG_DOCUMENT_6',
+                                    'FLAG_DOCUMENT_7','FLAG_DOCUMENT_8','FLAG_DOCUMENT_9',
+                                    'FLAG_DOCUMENT_10','FLAG_DOCUMENT_11','FLAG_DOCUMENT_12','FLAG_DOCUMENT_13',
+                                    'FLAG_DOCUMENT_14','FLAG_DOCUMENT_15','FLAG_DOCUMENT_16','FLAG_DOCUMENT_17',
+                                    'FLAG_DOCUMENT_18','FLAG_DOCUMENT_19','FLAG_DOCUMENT_20','FLAG_DOCUMENT_21',
+                                    'NFLAG_INSURED_ON_APPROVAL','NAME_CONTRACT_TYPE_x','CODE_GENDER',
+                                    'NAME_TYPE_SUITE_x','NAME_INCOME_TYPE','NAME_EDUCATION_TYPE','NAME_FAMILY_STATUS',
+                                    'NAME_HOUSING_TYPE','WEEKDAY_APPR_PROCESS_START_x','ORGANIZATION_TYPE','NAME_CONTRACT_TYPE_y',
+                                    'WEEKDAY_APPR_PROCESS_START_y','NAME_CASH_LOAN_PURPOSE','NAME_CONTRACT_STATUS','NAME_PAYMENT_TYPE',
+                                    'CODE_REJECT_REASON','NAME_TYPE_SUITE_y','NAME_CLIENT_TYPE','NAME_GOODS_CATEGORY',
+                                    'NAME_PORTFOLIO','NAME_PRODUCT_TYPE','CHANNEL_TYPE','NAME_SELLER_INDUSTRY',
+                                    'NAME_YIELD_GROUP','PRODUCT_COMBINATION']], drop_first=True)
+    mergeddf = pd.concat([mergeddf, dummy1], axis=1)
+    mergeddf.head()
+
+    mergeddf = mergeddf.drop(['FLAG_OWN_CAR','FLAG_OWN_REALTY','FLAG_MOBIL',
+                        'FLAG_EMP_PHONE','FLAG_WORK_PHONE','FLAG_CONT_MOBILE',
+                        'FLAG_PHONE','FLAG_EMAIL','REGION_RATING_CLIENT',
+                        'REGION_RATING_CLIENT_W_CITY','REG_REGION_NOT_LIVE_REGION',
+                        'REG_REGION_NOT_WORK_REGION','LIVE_REGION_NOT_WORK_REGION',
+                        'REG_CITY_NOT_LIVE_CITY','REG_CITY_NOT_WORK_CITY','FLAG_DOCUMENT_2',
+                        'FLAG_DOCUMENT_3','FLAG_DOCUMENT_4','FLAG_DOCUMENT_5','FLAG_DOCUMENT_6',
+                        'FLAG_DOCUMENT_7','FLAG_DOCUMENT_8','FLAG_DOCUMENT_9',
+                        'FLAG_DOCUMENT_10','FLAG_DOCUMENT_11','FLAG_DOCUMENT_12','FLAG_DOCUMENT_13',
+                        'FLAG_DOCUMENT_14','FLAG_DOCUMENT_15','FLAG_DOCUMENT_16','FLAG_DOCUMENT_17',
+                        'FLAG_DOCUMENT_18','FLAG_DOCUMENT_19','FLAG_DOCUMENT_20','FLAG_DOCUMENT_21',
+                        'NFLAG_INSURED_ON_APPROVAL','NAME_CONTRACT_TYPE_x','CODE_GENDER',
+                        'NAME_TYPE_SUITE_x','NAME_INCOME_TYPE','NAME_EDUCATION_TYPE','NAME_FAMILY_STATUS',
+                        'NAME_HOUSING_TYPE','WEEKDAY_APPR_PROCESS_START_x','ORGANIZATION_TYPE','NAME_CONTRACT_TYPE_y',
+                        'WEEKDAY_APPR_PROCESS_START_y','NAME_CASH_LOAN_PURPOSE','NAME_CONTRACT_STATUS','NAME_PAYMENT_TYPE',
+                        'CODE_REJECT_REASON','NAME_TYPE_SUITE_y','NAME_CLIENT_TYPE','NAME_GOODS_CATEGORY',
+                        'NAME_PORTFOLIO','NAME_PRODUCT_TYPE','CHANNEL_TYPE','NAME_SELLER_INDUSTRY',
+                        'NAME_YIELD_GROUP','PRODUCT_COMBINATION'], axis = 1)
+
+
+
+
+
+    logger.info('saving processed data')
+    mergeddf.to_csv(r'data/processed/merged.csv')
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
